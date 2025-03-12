84c0748b9804f4a82a6f49d1a34900ea481668fc
João Macaoli <devjoaom@gmail.com>
2025-03-12 11:19:38 -0300

    feat: add edit and delete icons to rooms items list

--- src/components/rooms/room-item.tsx
+++ src/components/rooms/room-item.tsx
@@ -1,4 +1,6 @@
 import { Room } from "@/app/types/room";
+import { Pencil, Trash2 } from "lucide-react";
+import Link from "next/link";
 
 export default function RoomItem({
   id, description, floor, capacity, status
@@ -16,10 +18,31 @@ export default function RoomItem({
         <h2 className="text-lg font-semibold">{description}</h2>
         <p className="text-sm text-gray-600">Andar: {floor}</p>
         <p className="text-sm text-gray-600">Capacidade: {capacity} pessoas</p>
+        <p className={`text-sm font-semibold
+          ${status === "active"
+            ? "text-green-600" : status === "inactive"
+              ? "text-gray-500" : "text-yellow-500"}`
+        }>
+          {status === "active" ? "Ativa" : status === "inactive" ? "Inativa" : "Em Manutenção"}
+        </p>
       </div>
-      <span className={`px-3 py-1 rounded text-white text-sm ${statusColors[status]}`}>
-        {status === "active" ? "Ativa" : status === "inactive" ? "Inativa" : "Manutenção"}
-      </span>
+
+      <div className="flex gap-2">
+        <Link
+          href={`/rooms/edit?id=${id}`}
+          className="bg-yellow-500 text-white px-3 py-1 rounded"
+        >
+          <Pencil />
+        </Link>
+
+        <Link
+          href={`#`}
+          // href={`/rooms/edit?id=${id}`}
+          className="bg-red-500 text-white px-3 py-1 rounded"
+        >
+          <Trash2 />
+        </Link>
+      </div>
     </div>
   );
 }
