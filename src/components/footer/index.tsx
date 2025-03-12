bd67ca80790218132c1e843bb8c5df9be73f05e6
João Macaoli <devjoaom@gmail.com>
2025-03-12 14:59:45 -0300

    feat: add all rights reserved on footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className=" bg-gray-800 text-white text-center p-4 mt-auto">
-      <p>© {new Date().getFullYear()} - Book My Room</p>
+      <p>© {new Date().getFullYear()} - Book My Room. Todos os direitos reservados.</p>
     </footer>
   );
 }
